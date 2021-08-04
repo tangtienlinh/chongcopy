424b22104596a03cad694f447f0a7d9c0a70ad6f
tangtienlinh <88389074+tangtienlinh@users.noreply.github.com>
2021-08-04 21:32:01 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://btc66.vn/'>https://btc66.vn/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Bạn không được copy, vui lòng về lại trang gốc : <a href='https://btc66.vn'>https://btc66.vn"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
